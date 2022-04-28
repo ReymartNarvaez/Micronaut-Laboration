7249139851f691aad83827c0a3d1c6e1fa065228
ReymartNarvaez <Reymart_narvaez@hotmail.com>
2022-04-28 10:27:39 +0200

    Added EmployeeController

---
+++ src/main/java/com/example/EmployeeController.java
@@ -0,0 +1,36 @@
+package com.example;
+
+import io.micronaut.core.annotation.NonNull;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.Post;
+import org.reactivestreams.Publisher;
+import reactor.core.publisher.Mono;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+
+import static io.micronaut.http.HttpStatus.CONFLICT;
+import static io.micronaut.http.HttpStatus.CREATED;
+
+@Controller("/employee")
+public class EmployeeController {
+
+    private final EmployeeRepository employeeRepository;
+
+    public EmployeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    @Get
+    Publisher<Employee> list() {
+        return employeeRepository.list();
+    }
+
+    @Post
+    Mono<HttpStatus> save(@NonNull @NotNull @Valid Employee employee) {
+        return employeeRepository.save(employee)
+                .map(add -> add ? CREATED : CONFLICT);
+    }
+}
