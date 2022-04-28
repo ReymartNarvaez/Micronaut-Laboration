161f10f25e7f7166a3d7d0b235cbc9717db470bd
ReymartNarvaez <Reymart_narvaez@hotmail.com>
2022-04-28 10:07:55 +0200

    Added employee class

---
+++ src/main/java/com/example/Employee.java
@@ -0,0 +1,24 @@
+package com.example;
+
+import io.micronaut.core.annotation.Introspected;
+
+@Introspected
+public class Employee {
+
+    private final String name;
+
+    private final String email;
+
+    public Employee(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
